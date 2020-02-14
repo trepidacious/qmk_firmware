30db87a68b114679dba78078e54ba785b38f3b3d
trepidacious <trepidacious@gmail.com>
2020-02-14 21:14:24 +0000

    Add bootloader key combo, switch to macos

--- keyboards/mountain_lizard_5/keymaps/default/keymap.c
+++ keyboards/mountain_lizard_5/keymaps/default/keymap.c
@@ -26,11 +26,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_MEDIA_NEXT_TRACK,
           KC_AUDIO_VOL_UP,
           KC_AUDIO_VOL_DOWN,
-          LCTL(LSFT(KC_P))
+          // LCTL(LSFT(KC_P))
+          LGUI(LSFT(KC_P))
+
           )
 };
 
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
     rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
+    // TODO pick mode based on keys pressed
+
+    matrix_scan();
+    wait_ms(10);
+    matrix_scan();
+
+    if (matrix_get_row(0) && matrix_get_row(1) && matrix_get_row(2)) {
+      // // Jump to bootloader.
+      bootloader_jump();
+    } else if (matrix_get_row(0)) {
+      set_unicode_input_mode(UC_WINC);
+    } else if (matrix_get_row(1)) {
+      set_unicode_input_mode(UC_LNX);
+    } else {
+      set_unicode_input_mode(UC_OSX);
+    }
+
 }
